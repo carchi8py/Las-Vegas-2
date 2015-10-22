80b41bc01207781e321aee51d869fabbd9aa3c9e
Chris Archibald <carchi8py@gmail.com>
2015-10-22 10:24:08 -0700

    Added our 2 objects. Will need to complete Location later.

---
+++ Las Vegas 2/Location.swift
@@ -0,0 +1,16 @@
+//
+//  Location.swift
+//  Las Vegas 2
+//
+//  Created by Chris Archibald on 10/22/15.
+//  Copyright © 2015 Chris Archibald. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+
+class Location: NSManagedObject {
+    
+}
---
+++ Las Vegas 2/Pin.swift
@@ -0,0 +1,44 @@
+//
+//  Pin.swift
+//  Las Vegas 2
+//
+//  Created by Chris Archibald on 10/22/15.
+//  Copyright © 2015 Chris Archibald. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import CoreData
+
+@objc(Pin)
+
+class Pin: NSManagedObject {
+    
+    struct Keys {
+        static let Latitude = "latitude"
+        static let Longitude = "longitude"
+    }
+    
+    @NSManaged var latitude: Double
+    @NSManaged var longitude: Double
+    @NSManaged var locations: [Location]
+    
+    override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+    
+    init(dictionary: [String : AnyObject], context: NSManagedObjectContext) {
+        
+        // Core Data
+        let entity =  NSEntityDescription.entityForName("Pin", inManagedObjectContext: context)!
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+        
+        // Dictionary
+        
+        latitude = dictionary[Keys.Latitude] as! Double
+        longitude = dictionary[Keys.Longitude] as! Double
+    }
+
+
+    
+}
