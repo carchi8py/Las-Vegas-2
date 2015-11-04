c498deb8c6e7dca60b48efdd5386673e0fd28cea
Chris Archibald <carchi8py@gmail.com>
2015-11-03 18:40:32 -0800

    Check in of Foursquare Location Core data object.

--- Las Vegas 2/Location.swift
+++ Las Vegas 2/Location.swift
@@ -13,4 +13,32 @@ import CoreData
 
 class Location: NSManagedObject {
     
+    @NSManaged var name: String
+    @NSManaged var latitude: Double
+    @NSManaged var longitude: Double
+    @NSManaged var url: String
+    @NSManaged var hereNow: Int
+    @NSManaged var totalCheckins: Int
+    @NSManaged var foursquareID: String
+    @NSManaged var pin: Pin
+    
+    override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+    
+    init(name: String, latitude: Double, longitude: Double, url:String, hereNow: Int, totalCheckins: Int, foursquareID: String, pin: Pin, context: NSManagedObjectContext) {
+        
+        let entity = NSEntityDescription.entityForName("Location", inManagedObjectContext: context)!
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+        
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.url = url
+        self.hereNow = hereNow
+        self.totalCheckins = totalCheckins
+        self.foursquareID = foursquareID
+        self.pin = pin
+    }
+    
 }
