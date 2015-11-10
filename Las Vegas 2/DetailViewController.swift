3f0b056ad2c3c9b67f9477a253ac3d8575d5afc4
Chris Archibald <carchi8py@gmail.com>
2015-11-09 17:52:17 -0800

    Created Detail View Controller

---
+++ Las Vegas 2/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  Las Vegas 2
+//
+//  Created by Chris Archibald on 11/9/15.
+//  Copyright © 2015 Chris Archibald. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
