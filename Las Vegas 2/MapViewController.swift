10cddd278bbdb4389a281d3c709d3c6e9b0278b1
Chris Archibald <carchi8py@gmail.com>
2015-11-09 17:11:37 -0800

    Crewing new tab for Map of location.

---
+++ Las Vegas 2/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  Las Vegas 2
+//
+//  Created by Chris Archibald on 11/9/15.
+//  Copyright © 2015 Chris Archibald. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import CoreData
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+}
